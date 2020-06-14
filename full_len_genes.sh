3bf9596f97f0c236d179a59f9f338ebd001d94a7
Lu Wang <65783528+wangL5@users.noreply.github.com>
2020-06-14 15:41:49 -0700

    Create full_len_genes.sh

---
+++ full_len_genes.sh
@@ -0,0 +1,7 @@
+#pull out full length genes from OG fasta files
+#add something to the end of each line
+#replace /n with nothing to get everything in one line
+#pull out partial=00
+#make new lines by replacing the first thing you added to each line with /n.
+
+sed 's/$/:/g' m_S1.fna | awk -v RS="" '{gsub (/\n/,"")}1' | awk -v RS="" '{gsub (/>N/,"\n>N")}1' | grep "partial=00" | sed 's/:/\n/g' | sed '/^$/d' > m_S1.fullgenes.fna
